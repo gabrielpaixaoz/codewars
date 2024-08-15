c62b84883e1b21b8f4daa3d70c2eba984c98140c
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-08-15 08:31:13 -0300

    kpm

---
+++ kyu6/roman numerals enconder.js
@@ -0,0 +1,20 @@
+let number = 1990
+
+
+function solution(number){
+let numbers = [1000,900,500,100,90,50,10,9,5,1]
+let separador = []
+
+numbers.forEach(element => {
+    let resto = number % element
+    debugger;
+    if(resto > element){
+        separador.push(element)
+    }
+    debugger;
+});
+debugger;
+
+}
+
+
