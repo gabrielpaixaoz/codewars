531f7cf9d8c7ce6cd26776a76a19e14a0713f2ce
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-08-15 10:00:01 -0300

    ikoljnlk

--- kyu6/roman numerals enconder.js
+++ kyu6/roman numerals enconder.js
@@ -5,10 +5,12 @@ let numbers = [1000,900,500,100,90,50,10,9,5,1]
 let separador = []
 numbers.forEach(element => {
     let resto = number % element
+    debugger
     if(resto != element){
         separador.push(element)
+        number == resto
+        debugger
     }
-    number == resto
     
 });
 console.log(separador)
