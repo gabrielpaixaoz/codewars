eb6e780bf1014a77c2f9409d7f6445970594a2ab
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-08-15 14:22:39 -0300

    Update roman numerals enconder.js

--- kyu6/roman numerals enconder.js
+++ kyu6/roman numerals enconder.js
@@ -3,8 +3,8 @@ function solution(number){
   var  roman = {M:1000,CM:900, D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1 };
     var ans = '';
   while(number>0){
-      for(a in roman){ 
-          if(roman[a]<=number){ ans += a; number-=roman[a]; break;}
+      for(i in roman){ 
+          if(roman[i]<=number){ ans += i; number-=roman[i]; break;}
           
       }
   }
