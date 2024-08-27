863ec7af381378f611de7133ec3d7c486800e5be
gabrielpaixaoz <gabrielpaix54@gmail.com>
2024-08-27 16:41:38 -0300

    daed

--- kyu5/string incrementer.js
+++ kyu5/string incrementer.js
@@ -1,25 +1,29 @@
-let strng = "foobar001"
+let strng = "fo99obar99"
 
 function incrementString (strng) {
 let resultado = strng.match(/\d+/g)
+console.log(resultado)
 if(resultado === null){
     return strng + "1"
 }
 else{
     let resultadopop = resultado.pop()
-    debugger
-    let resultadopop_number = Number(resultadopop) + 1 
-    debugger
-    let resultadopop_string = resultadopop_number.toString()
+    console.log(resultadopop)
     
-    let teste = strng.replace(resultadopop,'')
-    debugger
+    let resultadopop_number = (parseInt(resultadopop,10) + 1).toString().padStart(resultadopop.length, "0")
+    console.log(resultadopop_number)
+    let resultadopop_string = resultadopop_number.toString()
+    let regex = new RegExp(resultadopop + "(?=$)")
+    console.log(regex)
+
+    let teste = strng.replace(regex, "")
+    console.log(teste)
     let resultado_final = teste + resultadopop_string
     return resultado_final
 }
 
 }
 
-console.log(resultado)
+
 console.log(incrementString(strng))
 
