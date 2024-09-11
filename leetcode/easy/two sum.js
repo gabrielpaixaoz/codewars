82028bffe75e140da774c975acbaf1e5e4ac05a6
gabrielpaixaoz <gabrielpaix54@gmail.com>
2024-09-11 15:43:15 -0300

    daed

--- leetcode/easy/two sum.js
+++ leetcode/easy/two sum.js
@@ -3,12 +3,16 @@ let target = 9
 
 function twoSum(nums, target) {
     const hashTable = {};
+    debugger
     for (let i = 0; i < nums.length; i++) {
+        debugger
       const complement = target - nums[i];
       if (hashTable[complement] !== undefined) {
+        debugger
         return [hashTable[complement], i];
       }
       hashTable[nums[i]] = i;
+      debugger
     }
     return [];
   }
